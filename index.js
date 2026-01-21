ad101cccb3e949de7e4970530a23400982e9ddbf
harshaspatil15-maker <harshaspatil15@gmail.com>
2026-01-21 20:24:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.post("/save",(req,res) =>{
+                            const url = "mongodb://0.0.0.0:27017";
+                            const con = new MongoClient(url);
+                            const db = con.db("review6jan26");
+                            const coll = db.collection("review");
+                            const doc = {"name":req.body.name,"phone":req.body.phone,"review":req.body.text};
+                            coll.insertOne(doc)
+                           .then(response =>{
+                                         res.send(response);
+                           })
+                           .catch(error =>{
+                                        res.send(error);
+                          });
+      });
+
+
+
+app.listen(9000, () =>{
+              console.log("ready @ 9000");
+});
+
+
+ 
+                            
